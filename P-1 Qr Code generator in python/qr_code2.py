ec825cf98c3b293bf13cfefc6fe8a848a7557cd9
Suniksha <sunikshabenpatel@gmail.com>
2025-01-19 14:50:49 +0530

    Project - 1 Generating QR Code

--- P-1 Qr Code generator in python/qr_code2.py
+++ P-1 Qr Code generator in python/qr_code2.py
@@ -7,7 +7,7 @@ import qrcode.constants
 
 qr=qrcode.QRCode(version=1,
                  error_correction=qrcode.constants.ERROR_CORRECT_H,
-                 box_size=10,border=4)
+                 box_size=20,border=10)
 
 #add you url
 qr.add_data("https://www.youtube.com/@Wonder_women-l8l/videos")
@@ -16,7 +16,7 @@ qr.add_data("https://www.youtube.com/@Wonder_women-l8l/videos")
 qr.make(fit=True)
 
 #chnaged the color
-img = qr.make_image(fill_color="red",back_color="blue")
+img = qr.make_image(fill_color="pink",back_color="white")
 
 #save the image
-img.save("whoosh_tech_U2be.png")
+img.save("whoosh_tech_youtube.png")
