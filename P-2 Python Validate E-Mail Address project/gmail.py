ebc7a6ff7a13897dd3d8a699c266c2764e2f5b88
Suniksha <sunikshabenpatel@gmail.com>
2025-01-20 21:15:39 +0530

    Project - 2 E-mail validation writing conditions

--- P-2 Python Validate E-Mail Address project/gmail.py
+++ P-2 Python Validate E-Mail Address project/gmail.py
@@ -1,1 +1,15 @@
 #create a python file with extension .py
+
+email = input("Enter your Email : ") #g@g.in i have used 6 characters in this gmail
+
+#i got the string as an input 
+#Conditions
+
+if(len(email)>=6):
+    #pass
+    if(email[0].isalpha()):
+        pass
+    else:
+        print("WRONG EMAIL 2")
+else:
+    print("WRONG EMAIL 1 ")
