e92d85d4e61c2f793aef02e9239593a6775b3353
Suniksha <sunikshabenpatel@gmail.com>
2025-01-23 22:44:12 +0530

    Project - 2 E-mail validation giving if conditions

--- P-2 Python Validate E-Mail Address project/gmail.py
+++ P-2 Python Validate E-Mail Address project/gmail.py
@@ -4,7 +4,7 @@ email = input("Enter your Email : ") #g@g.in i have used 6 characters in this gm
 
 #i got the string as an input 
 #Conditions
-
+k=0,j=0,d=0
 if(len(email)>=6):
     #pass
     if(email[0].isalpha()):
@@ -14,7 +14,20 @@ if(len(email)>=6):
             #pass
             if(email[-4]==".") ^ (email[-3]=="."):
                 #pass
-                for()
+                for i in email:
+                    if i==i.isspace():
+                        k=1
+                    elif i.isalpha():
+                        if i==i.upper(): 
+                            j=1
+                    elif i.isdigit():
+                        continue
+                    elif i=="_" or i=="." or i=="@":
+                        continue
+                    else:
+                        d=1
+                if k==1 or j==1 or d==1:
+                    print("WRONG EMAIL 5");
             else:
                 print("WRONG EMAIL 4");
         else:
