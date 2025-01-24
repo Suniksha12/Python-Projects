a558a82f0c4af591c17720922eebbb6075c7b2e6
Suniksha <sunikshabenpatel@gmail.com>
2025-01-24 19:37:52 +0530

    Check Validate email in python using Regex

---
+++ P-2 Python Validate E-Mail Address project/gmail2.py
@@ -0,0 +1,13 @@
+"""Email Validation in Python Using Regex Function
+    a-Z
+    0-9
+    . _ time 1
+    @ time 1
+    . 2,3 index
+ """
+
+import re
+
+email_condition = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w
+
+
