cf2e788366711e9e891605e985973264e2c0d3ca
Suniksha <sunikshabenpatel@gmail.com>
2025-01-24 19:44:58 +0530

    Project - 2 Check Validate email in python using Regex

--- P-2 Python Validate E-Mail Address project/gmail2.py
+++ P-2 Python Validate E-Mail Address project/gmail2.py
@@ -8,6 +8,13 @@
 
 import re
 
-email_condition = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w
+email_condition = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+
+user_email = input(' Enter your Email : ')
+
+if re.search(email_condition,user_email):
+    print(" Right Email ")
+else:
+    print(" Wrong Email ")
 
 
