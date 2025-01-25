d8cd334ac6ce7462cfe1e9b79b1a917bae539c96
Suniksha <sunikshabenpatel@gmail.com>
2025-01-25 21:13:20 +0530

    Project - 3 follow command using bot

---
+++ P-3 Simplest Instagram Bot Python/instagram.py
@@ -0,0 +1,9 @@
+from instabot import Bot 
+# with this we can upload photo , follow and unfollow any person if we want
+bot = Bot()
+#login into the system
+bot.login(username="",password="")
+bot.follow('')
+
+#how to uplaod photo in insta for this put the image in the same directory
+
