742410423c0b57dba48d19807496c9fe06f967b9
Suniksha <sunikshabenpatel@gmail.com>
2025-01-27 16:31:56 +0530

    Project - 3 Unfollow a person using insta bot

--- P-3 Simplest Instagram Bot Python/instagram.py
+++ P-3 Simplest Instagram Bot Python/instagram.py
@@ -7,8 +7,6 @@
 
 # #how to uplaod photo in insta for this put the image in the same directory
 # """Also make sure you copy & paste the path and change the slash to /"""
-# # bot.upload_photo("C:\Users\sunik\OneDrive\Desktop\Python Projects\P-3 Simplest Instagram Bot Python")
-
 
 from instagrapi import Client
 
@@ -18,21 +16,22 @@ cl.login("nooob_businessman98", "sahimkhan123")
 # cl.user_follow(user_id)
 
 #Now i need to upalod photo onto the id
-"""Install the library pip install instagrapi"""
-from instagrapi import Client
-
-# Initialize the client
-# cl = Client()
-
-# # Login to Instagram
-# username = "your_username"
-# password = "your_password"
-# cl.login(username, password)
-
 # Path to the photo and caption
-photo_path = "WHOOSH.png"  # Replace with the path to your photo
-caption = "This is a test upload from a bot! 🚀 #PythonBot"
+# photo_path = "WHOOSH.png"  # Replace with the path to your photo
+# caption = "This is a test upload from a bot! 🚀 #PythonBot"
 
-# Upload the photo
-media = cl.photo_upload(photo_path, caption)
-print(f"Photo uploaded successfully: {media.dict()}")
+# # Upload the photo
+# media = cl.photo_upload(photo_path, caption)
+# print(f"Photo uploaded successfully: {media.dict()}")
+
+# """How to Unfollow the person"""
+# # Retrieve the user ID of the person you want to unfollow
+# user_id = cl.user_id_from_username("ishowspeed")
+
+# # Unfollow the user
+# cl.user_unfollow(user_id)
+# print(f"Successfully unfollowed the user with ID: {user_id}")
+
+
+
+
