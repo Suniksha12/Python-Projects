d74df78f8083f659e959ececd8d43287ee7950ce
Suniksha <sunikshabenpatel@gmail.com>
2025-01-31 00:38:17 +0530

    Project - 5 Creating the subject and the body

--- P-5 Send Multiple Emails/textmail.py
+++ P-5 Send Multiple Emails/textmail.py
@@ -4,3 +4,11 @@ ob = s.SMTP('smpt.gmail.com',587) # in this we give inside the server name and t
 ob.ehlo() #server function
 ob.starttls() #server connected
 
+ob.login('suni71561@gmail.com','sunikSha17@')
+
+#now we want to create the subject
+subject = "TEST"
+body = "I am a Student."
+message = "subject:{}\n\n{}".format(subject,body)
+listadd=['sunikshabenpatel@gmail.com',
+         'suniksha814@gmail.com']
