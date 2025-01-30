e66a0e6eb4b94ba2925b9f08b7b56da9b1318532
Suniksha <sunikshabenpatel@gmail.com>
2025-01-30 13:30:19 +0530

    Project - 4 performing reading and recognizing operation in voice assitant

--- P-4  Voice Assistant using Python/Voice_Assistant.py
+++ P-4  Voice Assistant using Python/Voice_Assistant.py
@@ -6,5 +6,19 @@ import datetime
 import pyjokes
 
 
-def sptext():
-    
+def sptext():                             #listening
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening....")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+        """Reading"""
+        try:
+            print("Recognizing...")
+            data = recognizer.recognize_google(audio)
+            print(data)
+        except sr.UnknownValueError:
+            print(" Not Understand ")
+
+#calling the function
+sptext()
