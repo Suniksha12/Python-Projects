71131c97447eca5b98d82627df70711327c9576e
Suniksha <sunikshabenpatel@gmail.com>
2025-02-01 19:09:16 +0530

    Project - 6 Defining a function

--- P-6 Typing Speed Test/type.py
+++ P-6 Typing Speed Test/type.py
@@ -1,6 +1,10 @@
 from time import *
 import random as r
 
+def mistake(partest,usertest):
+    error = 0
+    for i in range(len(partest)):
+        
 
 test = ["A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea.",
         "My name is Suniksha Patel","Welcome to Whoosh-tech You tube channel"]
@@ -9,4 +13,16 @@ test1 = r.choice(test)
 
 print("***** tying speed *****")
 print(test1)
+
+#Now we will take the user input
+#Now we will take 2 line breaks
+print()
+print()
+
+testinput = input(" Enter : ")
+
+"""Now we need Following Thing:
+   1) Speed Calculation
+   2) Error Checking
+   3) Function Defining"""
 
