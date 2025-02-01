0dd0c57eb10085cccbe1ede7c8be7a1d0ff7da4c
Suniksha <sunikshabenpatel@gmail.com>
2025-02-01 18:54:20 +0530

    Project - 6 testing the program in different Paragraph

--- P-6 Typing Speed Test/type.py
+++ P-6 Typing Speed Test/type.py
@@ -5,5 +5,8 @@ import random as r
 test = ["A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea.",
         "My name is Suniksha Patel","Welcome to Whoosh-tech You tube channel"]
 
+test1 = r.choice(test)
 
+print("***** tying speed *****")
+print(test1)
 
