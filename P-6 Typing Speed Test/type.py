3789480bc724cdbe68db4a753451329c673e8c08
Suniksha <sunikshabenpatel@gmail.com>
2025-02-01 21:36:39 +0530

    Project - 6 Defining a function speed_time

--- P-6 Typing Speed Test/type.py
+++ P-6 Typing Speed Test/type.py
@@ -4,9 +4,20 @@ import random as r
 def mistake(partest,usertest):
     error = 0
     for i in range(len(partest)):
-        
+        try:
+            if partest[i] != usertest[i]:
+                error = error + 1
+        except :
+            error = error + 1
+    return error
 
-test = ["A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea.",
+def speed_time(time_start, time_end, userinput):
+    time_delay = time_end - time_start
+    time_R = round(time_delay,2)
+    speed = len(userinput)/time_R
+    return round(speed)
+
+test = ["A paragraph is a self contained unit of discourse in writing dealing with a particular point or idea.",
         "My name is Suniksha Patel","Welcome to Whoosh-tech You tube channel"]
 
 test1 = r.choice(test)
@@ -18,11 +29,12 @@ print(test1)
 #Now we will take 2 line breaks
 print()
 print()
-
+time_1 = time()
 testinput = input(" Enter : ")
 
 """Now we need Following Thing:
    1) Speed Calculation
    2) Error Checking
    3) Function Defining"""
+time_2 = time()
 
