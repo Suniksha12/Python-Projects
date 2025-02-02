e6a4358f7aa581eb4e5486dd13040dadbaac0ac2
Suniksha <sunikshabenpatel@gmail.com>
2025-02-02 15:33:43 +0530

    Project - 7 defining function for google translator

--- P-7 Python Language Translation/google_trans.py
+++ P-7 Python Language Translation/google_trans.py
@@ -1,6 +1,19 @@
 from tkinter import *
 from tkinter import ttk
 
+#now we wil make the translator work
+from googletrans import Translator, LANGUAGES
+
+#defining a function for conversion
+def change(text="type",src="English",dest="Hindi"):
+    text1 = text
+    src1 = src
+    dest1 = dest
+    trans=Translator()
+    trans1 = trans.translate(text,src=src1,dest=dest1)
+    return trans1
+
+def data():
 root = Tk()
 root.title("Translator")
 root.geometry("500x700")
@@ -19,7 +32,7 @@ lab_txt.place(x=100,y=100,height=20,width=300)
 Sor_txt = Text(frame,font=("Time New Roman",40,"bold"),wrap=WORD)
 Sor_txt.place(x=10,y=130,height=150,width=480)
 
-list_text = [1,2,3,4]
+list_text = list(LANGUAGES.values())
 
 comb1_sor = ttk.Combobox(frame,value=list_text)
 comb1_sor.place(x=10,y=300,height=40,width=150)
