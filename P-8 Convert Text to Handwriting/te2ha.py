e0349d1f259f1da6e04f399950f3e6695622c324
Suniksha <sunikshabenpatel@gmail.com>
2025-02-03 00:44:20 +0530

    Project - 8 importing the required modules and the code

---
+++ P-8 Convert Text to Handwriting/te2ha.py
@@ -0,0 +1,6 @@
+import pywhatkit as pw
+
+txt="""Python is a versatile and powerful programming language known for its simplicity and readability. 
+It empowers developers to create everything from simple scripts to complex machine learning applications."""
+
+pw.text_to_handwriting()
