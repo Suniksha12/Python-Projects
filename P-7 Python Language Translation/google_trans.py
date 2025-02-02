e5547dd7ba9f0035042da8f68ebd73c41e7c238e
Suniksha <sunikshabenpatel@gmail.com>
2025-02-02 15:12:05 +0530

    Project - 7 translater button added

--- P-7 Python Language Translation/google_trans.py
+++ P-7 Python Language Translation/google_trans.py
@@ -19,6 +19,18 @@ lab_txt.place(x=100,y=100,height=20,width=300)
 Sor_txt = Text(frame,font=("Time New Roman",40,"bold"),wrap=WORD)
 Sor_txt.place(x=10,y=130,height=150,width=480)
 
+list_text = [1,2,3,4]
+
+comb1_sor = ttk.Combobox(frame,value=list_text)
+comb1_sor.place(x=10,y=300,height=40,width=100)
+comb1_sor.set("English")
+
+#we will make the button for language change
+button_change = Botton(frame,text="Translate",relief=RAISED)
+button_change.place(x=120,y=300,height=40,width=100)
+
+#destination combo box
+
 
 root.mainloop()
 
