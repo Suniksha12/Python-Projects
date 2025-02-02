d60160025db9c97d338f111a27fc6739af0ca706
Suniksha <sunikshabenpatel@gmail.com>
2025-02-02 14:54:46 +0530

    Project - 7 adding some graphical features

--- P-7 Python Language Translation/google_trans.py
+++ P-7 Python Language Translation/google_trans.py
@@ -2,6 +2,10 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
-
+root.title("Translator")
+root.geometry("500x700")
+root.config(bg='Red')
+lab_txt = Label(root,text="Translator",font=("Time New Roman",40,"bold"))
+lab_txt.place(x=100,y=40,height=50,width=300)
 root.mainloop()
 
