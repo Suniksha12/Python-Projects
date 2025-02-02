b4026198fec14a130d21a16501f882c13ea2889c
Suniksha <sunikshabenpatel@gmail.com>
2025-02-02 15:17:59 +0530

    Project - 7 completed adding the destination text

--- P-7 Python Language Translation/google_trans.py
+++ P-7 Python Language Translation/google_trans.py
@@ -22,15 +22,24 @@ Sor_txt.place(x=10,y=130,height=150,width=480)
 list_text = [1,2,3,4]
 
 comb1_sor = ttk.Combobox(frame,value=list_text)
-comb1_sor.place(x=10,y=300,height=40,width=100)
+comb1_sor.place(x=10,y=300,height=40,width=150)
 comb1_sor.set("English")
 
 #we will make the button for language change
-button_change = Botton(frame,text="Translate",relief=RAISED)
-button_change.place(x=120,y=300,height=40,width=100)
+button_change = Button(frame,text="Translate",relief=RAISED)
+button_change.place(x=170,y=300,height=40,width=150)
 
 #destination combo box
+comb1_dest = ttk.Combobox(frame,value=list_text)
+comb1_dest.place(x=330,y=300,height=40,width=150)
+comb1_dest.set("English")
 
+#making destination text box
+lab_txt = Label(root,text="Destination Text",font=("Time New Roman",20,"bold"),fg="Black",bg="Red")
+lab_txt.place(x=100,y=360,height=20,width=300)
+
+dest_txt = Text(frame,font=("Time New Roman",40,"bold"),wrap=WORD)
+dest_txt.place(x=10,y=400,height=150,width=480)
 
 root.mainloop()
 
