3519994437285f8a9d33e2b8a7438da282b00cba
Suniksha <sunikshabenpatel@gmail.com>
2025-02-04 12:35:10 +0530

    Project - 9 adding some label function

--- P-9 Digital Clock Using Python With Gui/digital.py
+++ P-9 Digital Clock Using Python With Gui/digital.py
@@ -10,6 +10,22 @@ clock.config(bg='yellow')
 #calling the label function
 lab_hr = Label(clock,text="00",font=('Time New Roman',60,"bold"),
                bg='red',fg="white")
-lab_hr.place()
+lab_hr.place(x=120,y=50,height=110,width=100)
+
+lab_hr_txt = Label(clock,text="Hour",font=('Time New Roman',60,"bold"),
+               bg='red',fg="white")
+lab_hr_txt.place(x=120,y=45,height=30,width=100)
+
+lab_min = Label(clock,text="00",font=('Time New Roman',60,"bold"),
+               bg='red',fg="white")
+lab_min.place(x=340,y=50,height=110,width=100)
+
+lab_sec = Label(clock,text="00",font=('Time New Roman',60,"bold"),
+               bg='red',fg="white")
+lab_sec.place(x=560,y=50,height=110,width=100)
+
+lab_am = Label(clock,text="00",font=('Time New Roman',60,"bold"),
+               bg='red',fg="white")
+lab_am.place(x=780,y=50,height=110,width=100)
 
 clock.mainloop()
