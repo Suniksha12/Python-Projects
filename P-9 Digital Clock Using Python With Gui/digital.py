5f392e98b68c1ec510c8c46ac20d71505691fd11
Suniksha <sunikshabenpatel@gmail.com>
2025-02-04 12:59:49 +0530

    Project - 9 defining label function in date

--- P-9 Digital Clock Using Python With Gui/digital.py
+++ P-9 Digital Clock Using Python With Gui/digital.py
@@ -1,16 +1,28 @@
 from tkinter import *
 import datetime 
 
+#print(datetime.datetime.now())
 
+def date_time():
+    time = datetime.datetime.now()
+    hr = time.strftime('%I')
+    mi = time.strftime('%M')
+    sec = time.strftime('%S')
+    am = time.strftime("%p")
+    lab_hr.config(text=hr)
+    lab_min.config(text=mi)
+    lab_sec.config(text=sec)
+    lab_am.config(text=am)
+    lab_hr.after(200,date_time)
 
 clock = Tk()
 
-#chanege the title
+#change the title
 clock.title('       ****Digital Clock****       ')
 clock.geometry('1000x500')
 clock.config(bg='yellow')
 
-#calling the label function
+#calling the label function time
 lab_hr = Label(clock,text="00",font=('Time New Roman',60,"bold"),
                bg='red',fg="white")
 lab_hr.place(x=120,y=50,height=110,width=100)
@@ -35,12 +47,23 @@ lab_sec_txt = Label(clock,text="Sec",font=('Time New Roman',20,"bold"),
                bg='red',fg="white")
 lab_sec_txt.place(x=560,y=190,height=40,width=100)
 
-lab_am = Label(clock,text="00",font=('Time New Roman',60,"bold"),
+lab_am = Label(clock,text="00",font=('Time New Roman',50,"bold"),
                bg='red',fg="white")
 lab_am.place(x=780,y=50,height=110,width=100)
 
 lab_am_txt = Label(clock,text="AM/PM",font=('Time New Roman',20,"bold"),
                bg='red',fg="white")
 lab_am_txt.place(x=780,y=190,height=40,width=100)
+
+#Calling the label function date
+lab_date = Label(clock,text="00",font=('Time New Roman',60,"bold"),
+               bg='red',fg="white")
+lab_date.place(x=120,y=50,height=110,width=100)
+
+lab_date_txt = Label(clock,text="Date",font=('Time New Roman',20,"bold"),
+               bg='red',fg="white")
+lab_date_txt.place(x=120,y=190,height=40,width=100)
+
+date_time()
 
 clock.mainloop()
