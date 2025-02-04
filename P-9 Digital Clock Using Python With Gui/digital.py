47dadce14acb556d15288a54ad8a873b6730729b
Suniksha <sunikshabenpatel@gmail.com>
2025-02-04 12:07:02 +0530

    Project - 9 additionally adding some features

--- P-9 Digital Clock Using Python With Gui/digital.py
+++ P-9 Digital Clock Using Python With Gui/digital.py
@@ -2,4 +2,14 @@ from tkinter import *
 
 clock = Tk()
 
+#chanege the title
+clock.title('       ****Digital Clock****       ')
+clock.geometry('1000x500')
+clock.config(bg='yellow')
+
+#calling the label function
+lab_hr = Label(clock,text="00",font=('Time New Roman',60,"bold"),
+               bg='red',fg="white")
+lab_hr.place()
+
 clock.mainloop()
