fa7e1428b5ddd285400d780c6b2fa0710002972b
Suniksha <sunikshabenpatel@gmail.com>
2025-02-06 00:31:07 +0530

    Project - 11 adding features

--- P-11 Python Internet Speed Project/internet_speed.py
+++ P-11 Python Internet Speed Project/internet_speed.py
@@ -2,5 +2,24 @@ from tkinter import *
 import speedtest
 
 sp = Tk()
+#adding features
+sp.title(" Internet Speed Test ")
+sp.geometry("500x500")
+sp.config(bg="Blue")
+
+lab = Label(sp,text = "Internet Speed Test", font=("Time New Roman",30,"bold"),bg="Blue",fg="white")
+lab.place(x=60,y=40)
+
+lab = Label(sp,text = "Download Speed", font=("Time New Roman",30,"bold"),bg="Blue",fg="white")
+lab.place(x=60,y=40)
+
+lab = Label(sp,text = "00", font=("Time New Roman",30,"bold"),bg="Blue",fg="white")
+lab.place(x=60,y=40)
+
+lab = Label(sp,text = "Uplaod Speed", font=("Time New Roman",30,"bold"),bg="Blue",fg="white")
+lab.place(x=60,y=40)
+
+lab = Label(sp,text = "00", font=("Time New Roman",30,"bold"),bg="Blue",fg="white")
+lab.place(x=60,y=40)
 
 sp.mainloop()
