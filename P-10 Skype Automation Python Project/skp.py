d2f01d146e7a15ca253a0e6fa3464a625ee09f35
Suniksha <sunikshabenpatel@gmail.com>
2025-02-05 11:34:26 +0530

    Project - 10 person can send messages

--- P-10 Skype Automation Python Project/skp.py
+++ P-10 Skype Automation Python Project/skp.py
@@ -3,7 +3,11 @@ from skpy import Skype
 slogin = Skype("sunikshabenpatel@gmail.com","sunikSha17@")
 
 #you can also give messages using the [] after contacts and giving thier skype ids 
-contact = slogin.contacts[]
+contact = slogin.contacts["live:.cid.2405d0ce58c715aa"]
+
+contact.chat.sendMsg("Welcome to Whoosh-Tech")
+
+
 
 # for i in contact:
 #     print(i)
