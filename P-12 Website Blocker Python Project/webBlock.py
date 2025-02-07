90150555bf8794b972272b38029b759c28b5891f
Suniksha <sunikshabenpatel@gmail.com>
2025-02-08 00:34:14 +0530

    Project - 12 Got the host file path

--- P-12 Website Blocker Python Project/webBlock.py
+++ P-12 Website Blocker Python Project/webBlock.py
@@ -3,4 +3,6 @@ import datetime
 
 end_time = datetime.datetime(2025,02,09)
 site_block = ["github.com/Suniksha12","www.linkedin.com/feed/"]
-host_path = ""
+host_path = "C:/Windows/System32/drivers/etc/hosts"
+redirect = "127.0.0.1"
+
