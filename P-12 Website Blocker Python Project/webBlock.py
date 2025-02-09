453ce0dc6fb1fbb20929c0c5ad48af32942e45cf
Suniksha <sunikshabenpatel@gmail.com>
2025-02-09 23:07:16 +0530

    Project - 12 using some file handling methods and loops

--- P-12 Website Blocker Python Project/webBlock.py
+++ P-12 Website Blocker Python Project/webBlock.py
@@ -8,6 +8,12 @@ redirect = "127.0.0.1"
 
 while True:
     if datetime.datetime.now()<end_time:
-        pass
+        print("start Blocking")
+        #we will use file handling 
+        with open(host_path,"r+") as host_file:
+            content = host_file.read()
+            for website in site_block:
+                if website not in content:
+                    host_file.write()
     else:
         pass
