96d49c5667ccd80dd9a15d476466678df3115c01
Suniksha <sunikshabenpatel@gmail.com>
2025-02-10 10:26:08 +0530

    Project - 13 functions added for os system

--- P-13 Code to shutdowwRestart your pc/shut.py
+++ P-13 Code to shutdowwRestart your pc/shut.py
@@ -1,6 +1,19 @@
 from tkinter import *
 import os
 
+#function for all the buttons
+def restart():
+    os.system("shutdown /r /t 1")
+
+def restart_time():
+    os.system("shutdown /r /t 20")
+
+def logout():
+    os.system("shutdown -1")
+
+def shutdown():
+    os.system("shutdown /s /t 1")
+
 st = Tk()
 
 #adding external features
