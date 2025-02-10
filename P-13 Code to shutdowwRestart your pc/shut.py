8b5418dc1b9b5404ddc9ed2340320447da9716ca
Suniksha <sunikshabenpatel@gmail.com>
2025-02-10 10:29:02 +0530

    functions integerated with the buttons

--- P-13 Code to shutdowwRestart your pc/shut.py
+++ P-13 Code to shutdowwRestart your pc/shut.py
@@ -25,19 +25,19 @@ st.config(bg="Blue")
 
 #we will include four button restart , logoff , and shutdown and restart with time
 # we will use button function for this
-r_button = Button(st,text="Restart",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="plus")
+r_button = Button(st,text="Restart",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="plus",command=restart)
 
 r_button.place(x=150,y=60,height=50, width=200)
 
-rt_button = Button(st,text="Restart Time",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="plus")
+rt_button = Button(st,text="Restart Time",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="plus",command=restart_time)
 
 rt_button.place(x=150,y=170,height=50, width=200)
 
-lg_button = Button(st,text="Log-Out",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="plus")
+lg_button = Button(st,text="Log-Out",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="plus",command=logout)
 
 lg_button.place(x=150,y=270,height=50, width=200)
 
-st_button = Button(st,text="ShutDown",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="plus")
+st_button = Button(st,text="ShutDown",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="plus",command=shutdown)
 
 st_button.place(x=150,y=370,height=50, width=200)
 
