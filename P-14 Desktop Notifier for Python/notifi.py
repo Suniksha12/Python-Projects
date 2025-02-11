3ddae2f3e9b0b32d905e871100175c5214049c3c
Suniksha <sunikshabenpatel@gmail.com>
2025-02-11 09:51:49 +0530

    Project - 14 importing modules and writing a main method

--- P-14 Desktop Notifier for Python/notifi.py
+++ P-14 Desktop Notifier for Python/notifi.py
@@ -1,1 +1,9 @@
 from plyer import notification
+import time
+
+if __name__ == '__main__':
+    notification.notify(
+        title="*** Take Rest ***",
+        message= "Rest is vital for better mental health, increased concentration and memory, a healthier immune system, reduced stress, improved mood and even a better metabolism",
+        
+    )
