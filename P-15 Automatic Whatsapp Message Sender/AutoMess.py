1caaeab40e97e1a7ca9274e1db4e8a087bdc2832
Suniksha <sunikshabenpatel@gmail.com>
2025-02-12 09:11:32 +0530

    Project - 15 completed the AutoMess.py of sending automatic message to the number provided in the code

---
+++ P-15 Automatic Whatsapp Message Sender/AutoMess.py
@@ -0,0 +1,5 @@
+import pywhatkit as pyw
+
+#below message will ask 4 parameter , first number , message
+#third add the time in hours, 4th is the minute
+pyw.sendwhatmsg('+919876543245','Welcome to Whoosh-Tech',09,14)
