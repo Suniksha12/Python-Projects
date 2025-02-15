7c8377ae45855364d94b42914bfd74b54339fd4a
Suniksha <sunikshabenpatel@gmail.com>
2025-02-15 09:40:15 +0530

    Project - 19 Completed SR.py

--- P-19 Build Screen Recorder in python/SR.py
+++ P-19 Build Screen Recorder in python/SR.py
@@ -14,8 +14,28 @@ f = cv2.VideoWriter_fourcss(*"XVID") #for XVID it will give mp4 ectc format
 
 output = cv2.VideoWriter("SR.mp4",f,30.0,dim) #giving the location in standard 30.0 frames per second is capture in a frame
 
-now_time = time.time()
+now_start_time = time.time()
 
 dur = 10+4 #suppose we have 10 sec then when the code is compiles 4 sec will be taken so 4 sec is added
 
 #we will give start time and then the end time
+end_time = now_start_time + dur 
+
+while True:
+    image = pyautogui.screenshot()
+    #create a framd and add the screen shots in that frame
+    frame_1 = np.array(image)
+    frame = cv2.cvtColor(frame_1,cv2.COLOR_BGR2RGB)
+
+    output.write(frame)
+
+    c_time = time.time()
+
+    #now stop the capturing
+    if c_time>end_time:
+        break
+
+#realease the video
+output.release()
+
+print("---END---")
