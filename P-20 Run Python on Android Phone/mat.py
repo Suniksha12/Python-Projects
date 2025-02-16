b59724b78918a5d97bfbe0aee3342b32a7a16891
Suniksha <sunikshabenpatel@gmail.com>
2025-02-16 19:02:17 +0530

    Project - 20 mat.py file for ploting the linear graph

---
+++ P-20 Run Python on Android Phone/mat.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4,5]
+
+y=[1,2,3,4,5]
+
+# we have many plots like lineplot graph plot , scatter plot etc 
+# now we will use linear plot 
+plt.plot(x,y)
+
+plt.show()
