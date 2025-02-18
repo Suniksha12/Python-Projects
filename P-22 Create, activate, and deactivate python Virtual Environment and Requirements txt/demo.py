73e08e58115494f4589dafdb51317b4f6ed1091e
Suniksha <sunikshabenpatel@gmail.com>
2025-02-18 09:57:03 +0530

    Project - 22 adding demo.py comments with some instructions

--- P-22 Create, activate, and deactivate python Virtual Environment and Requirements txt/demo.py
+++ P-22 Create, activate, and deactivate python Virtual Environment and Requirements txt/demo.py
@@ -4,3 +4,20 @@ print("Welcome to Whoosh-tech")
 
 #open your command prompt in the same directory where your code file is stored 
 #and write teh syntax : pip install virtualenv
+
+#then write 
+#Synatx: virtuaenv giveanyname
+
+#if your are gettting error as virtual env is not recognised then 
+#use this as python -m virtualenv giveanyname
+
+
+""" Open the filename then go inside the scripts and open command prompt
+by simply right clicking on it.
+
+then write the activate on your cmd and your file name virtual environmnet will be activated.
+and if its showing error then try to deactivate the file
+
+write deavtivate or change the file path ./activate
+
+or simply run as adminstrator and write set as administrator policies"""
