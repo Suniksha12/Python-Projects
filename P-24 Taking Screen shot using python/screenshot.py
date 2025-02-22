2efd0bd1f805f78e8613f5e6595e0b1b47df8a29
Suniksha <sunikshabenpatel@gmail.com>
2025-02-22 17:45:05 +0530

    Project - 25 GUI CREATED for the screenshot application

--- P-24 Taking Screen shot using python/screenshot.py
+++ P-24 Taking Screen shot using python/screenshot.py
@@ -1,8 +1,29 @@
+# import pyautogui
+
+
+# ss = pyautogui.screenshot()
+
+# #save the file
+# ss.save("test1.png")
+
+#in ths we will make a desktop application for this once you have run your program
+#ultimately you can find that your screenshot is being saved in the same directory 
+#where you have saved your program file
+
+#now in order to make an application we will require a module Tkinter 
+#open your command prompt and in it just put the command
+#syntax : pip install tkintertable
+
 import pyautogui
+from tkinter import *
 
-ss = pyautogui.screenshot()
+win = Tk()
+win.title("Win ScreenShot")
+win.geometry("400x300")
+win.config(bg="yellow")
+win.resizable(False, False)
 
-#save the file
-ss.save("test1.png")
+button = Button(win,text="Done", font=('Time New Roman',50,"bold"))
+button.place(x=100,y=100,height=100,width=200)
 
-#in ths we will make a desktop application
+win.mainloop()
