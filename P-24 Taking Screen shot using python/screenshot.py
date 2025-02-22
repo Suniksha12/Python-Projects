8a89b5fe18867945ef53b625c4fede29b9a0f329
Suniksha <sunikshabenpatel@gmail.com>
2025-02-22 17:53:56 +0530

    Project - 25 printing out the path and adding a done button and a url get box

--- P-24 Taking Screen shot using python/screenshot.py
+++ P-24 Taking Screen shot using python/screenshot.py
@@ -19,8 +19,12 @@ from tkinter import *
 
 #building a function
 def take_ss():
-    ss = pyautogui.screenshot()
-    ss.save("test1.png")
+    #how to get the data into entry box
+    add_data = entry.get()
+    path = add_data+"\\test.png"
+    print(path)
+    # ss = pyautogui.screenshot()
+    # ss.save("test1.png")
 
 win = Tk()
 win.title("Win ScreenShot")
@@ -31,7 +35,7 @@ win.resizable(False, False)
 entry = Entry(win, font=('Time New Roman',30))
 entry.place(x=20, height=70, width=660, y=50)
 
-button = Button(win,text="Done", font=('Time New Roman',50))
+button = Button(win,text="Done", font=('Time New Roman',50),command=take_ss)
 button.place(x=250,y=140,height=100,width=200)
 
 win.mainloop()
