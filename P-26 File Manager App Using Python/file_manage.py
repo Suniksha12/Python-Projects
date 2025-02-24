c9caaf4a3b77530651fea3319ff45da6444d4743
Suniksha <sunikshabenpatel@gmail.com>
2025-02-24 12:53:36 +0530

    Project - 26 fetched filename and extension

--- P-26 File Manager App Using Python/file_manage.py
+++ P-26 File Manager App Using Python/file_manage.py
@@ -5,4 +5,10 @@ path = input("Enter your path : ")
 files = os.listdir(path)
 
 for i in files:
-    print(i)
+    # print(i)
+    filename , extension = os.path.splitext(i)
+    # print(filename)
+    # #after printing the extension it will fetch with . and we need that to be removed
+    # print(extension)
+
+#With the files names fetches we want to seperated the file name and the extension
