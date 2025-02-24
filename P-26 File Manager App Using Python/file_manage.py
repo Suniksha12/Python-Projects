e982df68c4b07b70bb1a7604e3a12730d0ac8c22
Suniksha <sunikshabenpatel@gmail.com>
2025-02-24 12:59:17 +0530

    Project - 26 checking if the folder exists or not

--- P-26 File Manager App Using Python/file_manage.py
+++ P-26 File Manager App Using Python/file_manage.py
@@ -10,5 +10,13 @@ for i in files:
     # print(filename)
     # #after printing the extension it will fetch with . and we need that to be removed
     # print(extension)
-
+    #In the below statemnet we doing slicing of the string
+    extension_1 = extension[1:]
+    # print(extension_1)
+    #with the  help of this extension we will make a folder
+    folder_path = path+"\\"+extension_1
+    if(os.path.exists(folder_path)):
+        print("True")
+    else :
+        print("False")
 #With the files names fetches we want to seperated the file name and the extension
