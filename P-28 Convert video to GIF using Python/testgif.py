143ed7c0e2c2bcbf2f5158e8c1d1de32d37dfbef
Suniksha <sunikshabenpatel@gmail.com>
2025-02-25 10:56:38 +0530

    Project - 28 completed the rotated function in GIF

--- P-28 Convert video to GIF using Python/testgif.py
+++ P-28 Convert video to GIF using Python/testgif.py
@@ -1,5 +1,9 @@
 from moviepy.editor import *
 
 #add your path below
-video = VideoFileClip("").subclip(00,5)
-video.write_gif("test1.gif")
+# video = VideoFileClip("").subclip(00,5)
+# video.write_gif("test1.gif")
+
+#if we want to make the GIF rotate for which we will be needed 
+video = VideoFileClip("").subclip(00,5).rotate(180)
+video.write_gif("test2.gif")
