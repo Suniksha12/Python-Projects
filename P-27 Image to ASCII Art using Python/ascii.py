15535ce0639191d557f3b562fa87d3946666270c
Suniksha <sunikshabenpatel@gmail.com>
2025-02-25 10:17:44 +0530

    Project - 27 importing modules and generating ascii images

--- P-27 Image to ASCII Art using Python/ascii.py
+++ P-27 Image to ASCII Art using Python/ascii.py
@@ -1,3 +1,4 @@
 import pywhatkit as kit
 
-kit.image_to_ascii_art()
+kit.image_to_ascii_art('C:/Users/sunik/OneDrive/Desktop/Python Projects/P-27 Image to ASCII Art using Python/WHOOSH.png',"ascii.txt")
+
