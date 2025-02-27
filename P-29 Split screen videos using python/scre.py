aa0469c5b14c1c95b5edd9620cad08e7f39da0cb
Suniksha <sunikshabenpatel@gmail.com>
2025-02-27 14:03:48 +0530

    Project - 29 done with screen spliting video using python

--- P-29 Split screen videos using python/scre.py
+++ P-29 Split screen videos using python/scre.py
@@ -2,6 +2,10 @@ from moviepy.editor import *
 
 clip_1 = VideoFileClip("demo1.mp4").subclip(0,10)
 clip_2 = VideoFileClip("demo2.mp4").subclip(10,20)
-clip_3 = VideoFileClip("demo3.mp4").subclip(00,10)
-clip_4 = VideoFileClip("demo4.mp4").subclip(00,10)
+clip_3 = VideoFileClip("demo3.mp4").subclip(10,20)
+clip_4 = VideoFileClip("demo4.mp4").subclip(10,20)
 
+combine = clips_array([[clip_1,clip_2],
+                       [clip_3,clip_4]])
+
+combine.write_videoofile("test.mp4")
