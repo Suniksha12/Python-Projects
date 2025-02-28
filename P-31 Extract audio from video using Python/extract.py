7ee6f12e8961b2b6e14513afd7054c216fa495bf
Suniksha <sunikshabenpatel@gmail.com>
2025-02-28 18:14:14 +0530

    Project - 31 created second audio file

--- P-31 Extract audio from video using Python/extract.py
+++ P-31 Extract audio from video using Python/extract.py
@@ -1,4 +1,7 @@
 from moviepy.editor import *
 
-clip1 = VideoFileClip("test1.mp4").subclip(0,20)
-clip1.audio.write_audiofile("test1.mp3")
+# clip1 = VideoFileClip("test1.mp4").subclip(0,20)
+# clip1.audio.write_audiofile("test1.mp3")
+
+clip1 = VideoFileClip("test2.mp4").subclip(5,25)
+clip1.audio.write_audiofile("test2.mp3")
