e96d8e13182e3715dbf2fb211d0fd4966c022181
Suniksha <sunikshabenpatel@gmail.com>
2025-02-28 18:25:14 +0530

    Project - 31 create a single final video

--- P-31 Extract audio from video using Python/extract.py
+++ P-31 Extract audio from video using Python/extract.py
@@ -1,5 +1,13 @@
 from moviepy.editor import *
 
+main_video = VideoFileClip("test2.mp4").subclip(10,30)
+main_video = main_video.without_audio()
+
+main_audio = AudioFileClip("test1.mp3")
+
+final_video = main_video.set_audio(main_audio)
+final_video.write_videofile("finalvideo.mp4")
+
 # clip1 = VideoFileClip("test1.mp4").subclip(0,20)
 # clip1.audio.write_audiofile("test1.mp3")
 
@@ -10,8 +18,8 @@ from moviepy.editor import *
 # clip1 = clip1.without_audio()
 # clip1.write_videofile("test_w1.mp4")
 
-video_file = VideoFileClip("test_w1.mp4")
-audio_file = AudioFileClip("test2.mp3")
+# video_file = VideoFileClip("test_w1.mp4")
+# audio_file = AudioFileClip("test2.mp3")
 
-final_video = video_file.set_audio(audio_file)
-final_video.write_videofile("test_new1.mp4")
+# final_video = video_file.set_audio(audio_file)
+# final_video.write_videofile("test_new1.mp4")
