626dd6fe13b03444d2dd31dd83986d9d18ddd068
Suniksha <sunikshabenpatel@gmail.com>
2025-02-28 18:17:13 +0530

    Project - 31 creating video without audio

--- P-31 Extract audio from video using Python/extract.py
+++ P-31 Extract audio from video using Python/extract.py
@@ -3,5 +3,9 @@ from moviepy.editor import *
 # clip1 = VideoFileClip("test1.mp4").subclip(0,20)
 # clip1.audio.write_audiofile("test1.mp3")
 
-clip1 = VideoFileClip("test2.mp4").subclip(5,25)
-clip1.audio.write_audiofile("test2.mp3")
+# clip1 = VideoFileClip("test2.mp4").subclip(5,25)
+# clip1.audio.write_audiofile("test2.mp3")
+
+clip1 = VideoFileClip("test1.mp4").subclip(0,20)
+clip1 = clip1.without_audio()
+clip1.write_videofile("test_w1.mp4")
