07638267590961357af4c8ed8ff6fa237ddb8d2e
Suniksha <sunikshabenpatel@gmail.com>
2025-03-01 18:53:36 +0530

    Project - 32 full code completed

--- P-32 Merge Many Videos in one using Python/merge.py
+++ P-32 Merge Many Videos in one using Python/merge.py
@@ -8,3 +8,5 @@ clip2 = clip2.set_position((45,150))
 final_video = concatenate_videoclips([clip1,clip2])
 
 final_video.write_videofile("new_test.mp4")
+
+#final video will be created but it will take some time wait for it.
