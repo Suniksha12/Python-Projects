d630be80314a1059f17c1fe4922490fc1ec6f1ee
Suniksha <sunikshabenpatel@gmail.com>
2025-03-02 11:08:39 +0530

    Project - 33 snakegame.py created in whihc i created the windows for the game

---
+++ P-33 Building snake game with pygame using python/snakegame.py
@@ -0,0 +1,19 @@
+import pygame
+import time 
+import random
+
+pygame.init()
+
+#obtain colors
+red = (255,0,0)
+blue = (51,15,255)
+grey = (192,192,192)
+green = (51,102,0)
+
+#display define
+win_width = 600
+win_height = 400
+
+window = pygame.display.set_mode((win_width,win_height))
+
+
