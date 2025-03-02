a3c719d10dcf2ae804725b57253275023ebfd586
Suniksha <sunikshabenpatel@gmail.com>
2025-03-02 11:32:02 +0530

    Project - 33 adding message and few functions

--- P-33 Building snake game with pygame using python/snakegame.py
+++ P-33 Building snake game with pygame using python/snakegame.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 import time 
 import random
 
@@ -31,6 +32,43 @@ def user_score(score):
 def game_snake():
     pass
 
+def message(msg):
+    msg = font_style.render(msg,True,red)
+    window.blit(msg,[win_width/6,win_height/3])
+
+def game_loop():
+    gameOver = False
+    gameClose = False
+
+    x1 = win_width/2
+    y1 = win_height/2
+
+    x1_change =0
+    y1_change =0
+
+    snake_length_list =[]
+    snake_length = 1
+
+    foodx = round(random.randrange(0,win_width - snake)/10.0) * 10.0
+    foody = round(random.randrange(0,win_height-snake)/10.0) * 10.0
+
+    while not gameOver:
+        while gameClose == True:
+            window.fill(grey)
+            message("You lost !! Press P to play the game again and Q  to quit the game")
+            user_score(snake_length-1)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        gameOver = True
+                        gameClose = True
+                    if event.key == pygame.K_q:
+                        game_loop()
+
+        for event in pygame.event.get():
+            if(event.type == pygame.KEYDOWN):
 
 # fonts = pygame.font.get_fonts()
 # print(fonts)
