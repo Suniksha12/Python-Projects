adb1ad08bf4b35c6913e969e5fe83722f38b3a17
Suniksha <sunikshabenpatel@gmail.com>
2025-03-04 10:11:39 +0530

    Project - 34 completed the rotation code and got output as pdf got rotated

--- P-35 Rotate Pdf files in python/pdfRotate.py
+++ P-35 Rotate Pdf files in python/pdfRotate.py
@@ -1,4 +1,9 @@
 import pikepdf
 
-pdf_my = pikepdf.PDF.open("C:\Users\sunik\OneDrive\Desktop\Python Projects\P-35 Rotate Pdf files in python\PHY1001_ENGINEERING-PHYSICS_LTP_1.0_1_PHY1001-Engineering Physics.pdf")
+pdf_my = pikepdf.Pdf.open("C:/Users/sunik/OneDrive/Desktop/Python Projects/P-35 Rotate Pdf files in python/PHY1001_ENGINEERING-PHYSICS_LTP_1.0_1_PHY1001-Engineering Physics.pdf")
 
+# print(pdf_my.pages)
+
+for i in pdf_my.pages:
+    i.Rotate = 180
+    pdf_my.save("new_pdf.pdf")
