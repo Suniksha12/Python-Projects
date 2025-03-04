b3b4a197e11d5816e25b7e27ad1412d2bf00a26b
Suniksha <sunikshabenpatel@gmail.com>
2025-03-04 10:05:09 +0530

    Project - 34 Added pdfRotate.py with importing modules and providing the access to file path

---
+++ P-35 Rotate Pdf files in python/pdfRotate.py
@@ -0,0 +1,4 @@
+import pikepdf
+
+pdf_my = pikepdf.PDF.open("C:\Users\sunik\OneDrive\Desktop\Python Projects\P-35 Rotate Pdf files in python\PHY1001_ENGINEERING-PHYSICS_LTP_1.0_1_PHY1001-Engineering Physics.pdf")
+
