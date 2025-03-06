027e06f1baa3d6cefe48e96bd684e78506c2f75b
Suniksha <sunikshabenpatel@gmail.com>
2025-03-06 21:40:22 +0530

    Project - 37 Adding the logic of winnning and losing while adding the comments

--- P-37 Build Hangman Game using python/hangman.py
+++ P-37 Build Hangman Game using python/hangman.py
@@ -7,10 +7,25 @@ total_chances = 7
 
 guessed_word = "-"*len(word)
 
-print(guessed_word)
-letter = input("Guess a Letter: ").upper()
-if letter in word:
-    for index in range(len(word)):
-        if word[index] == letter:
-            guessed_word = guessed_word[:index]+letter+guessed_word[index+1:]
-            print(guessed_word)
+while(total_chances) != 0:
+    print(guessed_word)
+    letter = input("Guess a Letter: ").upper()
+    if letter in word:
+        for index in range(len(word)):
+            if word[index] == letter:
+                guessed_word = guessed_word[:index]+letter+guessed_word[index+1:]
+                # print(guessed_word)
+    
+        if guessed_word == word:
+            print("Congratulations you won!!!")
+            break
+
+    else:
+        total_chances -= 1
+        print("Incorrect guess")
+        print("the remaining chances are: ",total_chances)
+else:
+    print("Game Over")
+    print("You Lose")
+    print("All the chances are exhausted")
+print("The correct word is: ",word)
