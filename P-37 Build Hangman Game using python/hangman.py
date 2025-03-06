3df8da622015ebe46146ebc26f13fae719c877a4
Suniksha <sunikshabenpatel@gmail.com>
2025-03-06 21:22:35 +0530

    Project - 37 Adding the correct guessing number

--- P-37 Build Hangman Game using python/hangman.py
+++ P-37 Build Hangman Game using python/hangman.py
@@ -4,3 +4,13 @@ words = ["UMBRELLA", "COMPUTER","TELESCOPE","SMARTPHONE"]
 word = random.choice(words)
 
 total_chances = 7
+
+guessed_word = "-"*len(word)
+
+print(guessed_word)
+letter = input("Guess a Letter: ").upper()
+if letter in word:
+    for index in range(len(word)):
+        if word[index] == letter:
+            guessed_word = guessed_word[:index]+letter+guessed_word[index+1:]
+            print(guessed_word)
