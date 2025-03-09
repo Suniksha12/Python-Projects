63ef569805fd932aceb1ad50d9410aba21975238
Suniksha <sunikshabenpatel@gmail.com>
2025-03-09 14:13:59 +0530

    Project -39 made the simple to do list notification

--- P-39 Build a desktop notifier for displaying stock market data/notifi.py
+++ P-39 Build a desktop notifier for displaying stock market data/notifi.py
@@ -2,9 +2,13 @@ import datetime
 import time
 from plyer import notification
 
-notification.notify(
-    title = "To-Do List".format(datetime.date.today()),
-    message = "1. live classes \n2. projects \n3. presentation"
-)
+while True :
+    notification.notify(
+        title = "To-Do List".format(datetime.date.today()),
+        message = "1. live classes \n2. projects \n3. presentation",
+        app_icon = (r"C:\Users\sunik\OneDrive\Desktop\Python Projects\P-39 Build a desktop notifier for displaying stock market data\notifi.png"),
+        timeout = 10
+    )
+    time.sleep(10)
 
 #for the icons parts go to the link provided - https://www.iconarchive.com/search?q=application+.ico&page=7
