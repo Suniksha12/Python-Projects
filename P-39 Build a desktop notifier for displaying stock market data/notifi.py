1bb5d2c39716763c48df66895f4f1b422cc5d213
Suniksha <sunikshabenpatel@gmail.com>
2025-03-09 14:20:25 +0530

    Project -39 added some modifications and .ico file

--- P-39 Build a desktop notifier for displaying stock market data/notifi.py
+++ P-39 Build a desktop notifier for displaying stock market data/notifi.py
@@ -2,12 +2,12 @@ import datetime
 import time
 from plyer import notification
 
-while True :
+while True:
     notification.notify(
-        title = "To-Do List".format(datetime.date.today()),
-        message = "1. live classes \n2. projects \n3. presentation",
-        app_icon = (r"C:\Users\sunik\OneDrive\Desktop\Python Projects\P-39 Build a desktop notifier for displaying stock market data\notifi.png"),
-        timeout = 10
+        title="To-Do List ({})".format(datetime.date.today()),  # Fixed title formatting
+        message="1. Live classes \n2. Projects \n3. Presentation",
+        app_icon=r"C:\Users\sunik\OneDrive\Desktop\Python Projects\P-39 Build a desktop notifier for displaying stock market data\Alecive-Flatwoken-Apps-Notifications.ico",  # Use .ico
+        timeout=10
     )
     time.sleep(10)
 
