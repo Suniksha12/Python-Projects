7e20a656954667db913786f6075460e5789c77d9
Suniksha <sunikshabenpatel@gmail.com>
2025-03-09 14:11:54 +0530

    Project - 38 added notification part

--- P-39 Build a desktop notifier for displaying stock market data/notifi.py
+++ P-39 Build a desktop notifier for displaying stock market data/notifi.py
@@ -6,3 +6,5 @@ notification.notify(
     title = "To-Do List".format(datetime.date.today()),
     message = "1. live classes \n2. projects \n3. presentation"
 )
+
+#for the icons parts go to the link provided - https://www.iconarchive.com/search?q=application+.ico&page=7
