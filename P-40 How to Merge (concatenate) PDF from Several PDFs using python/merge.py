b8e92945ec1e7ee8165a9936c68cb5e014f87650
Suniksha <sunikshabenpatel@gmail.com>
2025-03-10 22:22:55 +0530

    Project - 40 completed the mergepdf.py

---
+++ P-40 How to Merge (concatenate) PDF from Several PDFs using python/merge.py
@@ -0,0 +1,11 @@
+from glob import glob
+from pikepdf import Pdf
+
+new_pdf = Pdf.new()
+
+for file in glob("*.pdf"):
+    old_pdf = Pdf.open(file)
+    new_pdf.pages.extend(old_pdf.pages)
+
+#save the new_pdf
+new_pdf.save("demo.pdf")
