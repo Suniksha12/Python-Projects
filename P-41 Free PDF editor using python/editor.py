6d8f62a5ca2870491ac867b91f873eea2b3762c8
Suniksha <sunikshabenpatel@gmail.com>
2025-03-11 20:51:24 +0530

    Project - 41 changed the destination of pages

--- P-41 Free PDF editor using python/editor.py
+++ P-41 Free PDF editor using python/editor.py
@@ -7,5 +7,9 @@ old_pdf = pikepdf.Pdf.open(r"C:\Users\sunik\OneDrive\Desktop\Python Projects\P-4
 # old_pdf.save("rev_new.pdf")
 
 #deleting any page from our pdf
-del old_pdf.pages[1:2]
-old_pdf.save("del_new.pdf")
+# del old_pdf.pages[1:2]
+# old_pdf.save("del_new.pdf")
+
+#changing the destination of pages
+old_pdf.pages[2] = old_pdf.pages[1]
+old_pdf.save("rep_new.pdf")
