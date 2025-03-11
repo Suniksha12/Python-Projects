05f581a9af3fb893a598f244fd806fca64ac54c1
Suniksha <sunikshabenpatel@gmail.com>
2025-03-11 20:48:57 +0530

    Project - 41 pdf page deleted

--- P-41 Free PDF editor using python/editor.py
+++ P-41 Free PDF editor using python/editor.py
@@ -3,5 +3,9 @@ import pikepdf
 old_pdf = pikepdf.Pdf.open(r"C:\Users\sunik\OneDrive\Desktop\Python Projects\P-41 Free PDF editor using python\PHY1001_ENGINEERING-PHYSICS_LTP_1.0_1_PHY1001-Engineering Physics.pdf")
 
 #reversing the old pdf
-old_pdf.pages.reverse()
-old_pdf.save("rev_new.pdf")
+# old_pdf.pages.reverse()
+# old_pdf.save("rev_new.pdf")
+
+#deleting any page from our pdf
+del old_pdf.pages[1:2]
+old_pdf.save("del_new.pdf")
