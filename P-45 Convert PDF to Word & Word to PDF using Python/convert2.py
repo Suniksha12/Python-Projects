33cdf4ec848d994906ad7ff856d8b1601f52aa15
Suniksha <sunikshabenpatel@gmail.com>
2025-03-15 16:17:41 +0530

    Project - 45 Converted doc file inot .pdf file

--- P-45 Convert PDF to Word & Word to PDF using Python/convert2.py
+++ P-45 Convert PDF to Word & Word to PDF using Python/convert2.py
@@ -1,3 +1,3 @@
 from docx2pdf import convert
 
-convert("new.docx",r"C:\Users\sunik\OneDrive\Desktop\Python Projects\P-45 Convert PDF to Word & Word to PDF using Python\PHY1001_ENGINEERING-PHYSICS_LTP_1.0_1_PHY1001-Engineering Physics.pdf")
+convert("new.docx","new_pdf.pdf")
