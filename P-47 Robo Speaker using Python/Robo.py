b85640214d1eb360ff2ac871a9186e278be5d4e0
Suniksha <sunikshabenpatel@gmail.com>
2025-03-17 18:49:27 +0530

    Project - 47 Added Robo.py with all the files needed

--- P-47 Robo Speaker using Python/Robo.py
+++ P-47 Robo Speaker using Python/Robo.py
@@ -1,7 +1,15 @@
-import os
+import pyttsx3
 
 if __name__ == '__main__':
     print("Welcome to Robo Speaker 1.1 Created by Whossh-tech")
-    x = input("Enter What do you want me to speak: ")
-    command = f"say {x}"
-    os.system(command)
+    
+    engine = pyttsx3.init()
+    
+    while True:
+        x = input("Enter what you want me to speak (or type 'exit' to quit): ")
+        if x.lower() == "exit":
+            print("Goodbye!")
+            break
+
+        engine.say(x)
+        engine.runAndWait()
