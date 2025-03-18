e1008eda09a1f9dd992d8fe2efcf268246805660
Suniksha <sunikshabenpatel@gmail.com>
2025-03-18 18:59:05 +0530

    Project - 48 adding current date and time with resize and frame

--- P-48 Facial Recognition Attendance System using python/Attendance_system.py
+++ P-48 Facial Recognition Attendance System using python/Attendance_system.py
@@ -19,3 +19,22 @@ rohan_encoding = face_recognition.face_encodings(rohan_iamge)[0]
 
 known_face_encodings = [suni_encoding,rohan_encoding]
 known_face_names = ["Suni","Rohan"]
+
+#list of expected students
+students = known_face_names.copy()
+
+face_locations = []
+face_encodings = []
+
+#Get the current date and time
+now = datetime.now()
+# current_date = datetime.now()
+current_date = now.strftime("%Y-%m-%d")
+
+f= open(f"{current_date}.csv","w+" , newline="")
+lnwriter = csv.writer(f)
+
+while True:
+    _, frame = video_capture.read()
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25 , fy=0.25)
+    
