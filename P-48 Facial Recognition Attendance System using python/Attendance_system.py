b0162bab94f11bcfec226d77291b96c0245f6b4b
Suniksha <sunikshabenpatel@gmail.com>
2025-03-18 19:03:16 +0530

    Project - 48 added for loop for encoding

--- P-48 Facial Recognition Attendance System using python/Attendance_system.py
+++ P-48 Facial Recognition Attendance System using python/Attendance_system.py
@@ -37,4 +37,14 @@ lnwriter = csv.writer(f)
 while True:
     _, frame = video_capture.read()
     small_frame = cv2.resize(frame, (0, 0), fx=0.25 , fy=0.25)
-    
+    rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
+    #Recognize faces
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+    for face_encoding in face_encodings:
+        mathes = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        face_distance = face_recognition.face_distance(known_face_encodings,
+                                                       face_encoding)
+        best_match_index = np.argmin(face_distance)
