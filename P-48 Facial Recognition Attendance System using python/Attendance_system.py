5cdf5ebb971883ed34aa4675f44d7a77d812fed1
Suniksha <sunikshabenpatel@gmail.com>
2025-03-18 19:07:25 +0530

    Project - 48 added the if condition with capturing windows feature

--- P-48 Facial Recognition Attendance System using python/Attendance_system.py
+++ P-48 Facial Recognition Attendance System using python/Attendance_system.py
@@ -44,7 +44,18 @@ while True:
     face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
     for face_encoding in face_encodings:
-        mathes = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
         face_distance = face_recognition.face_distance(known_face_encodings,
                                                        face_encoding)
         best_match_index = np.argmin(face_distance)
+
+        if(matches[best_match_index]):
+            name = known_face_names[best_match_index]
+
+    cv2.imshow("Attendance", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
+f.close()
