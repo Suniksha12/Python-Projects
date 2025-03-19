b4f6d8471463fbdc9278be681a06af4bf71e9b52
Suniksha <sunikshabenpatel@gmail.com>
2025-03-19 18:09:14 +0530

    Project - 49 generating the random value

--- P-49 Rock Paper Scissor using Python/RPS.py
+++ P-49 Rock Paper Scissor using Python/RPS.py
@@ -3,9 +3,18 @@ import random
 user_wins = 0
 computer_wins = 0
 
+options = ["rock", "paper", "scissors"]
+
 while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit.").lower()
     if(user_input == 'q'):
-        quit()
+        break
 
-    
+    if user_input not in options:
+        continue
+
+    random_number = random.randint(0,2)
+    # rock: 0, paper: 1, scissor: 2
+
+
+print("Goodbye!")
