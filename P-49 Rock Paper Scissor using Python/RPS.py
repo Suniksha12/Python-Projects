c1936a1f06e8e60913b90559363b68144ca040de
Suniksha <sunikshabenpatel@gmail.com>
2025-03-19 18:14:18 +0530

    Project - 49 adding the if statement

--- P-49 Rock Paper Scissor using Python/RPS.py
+++ P-49 Rock Paper Scissor using Python/RPS.py
@@ -15,6 +15,11 @@ while True:
 
     random_number = random.randint(0,2)
     # rock: 0, paper: 1, scissor: 2
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
 
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You Win!")
+        user_wins += 1
 
 print("Goodbye!")
